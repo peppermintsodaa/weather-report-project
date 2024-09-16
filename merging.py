746729a60071e3c9ea937044a1963fa20ad4e87b
s4002620 <s4002620@student.rmit.edu.au>
2024-09-16 21:02:00 +1000

    new merging

--- merging.py
+++ merging.py
@@ -4,21 +4,22 @@ import pandas as pd
 ica_data = pd.read_csv('cosc2669-or-cosc2186-WIL-project/datasets_cleaned/cleaned_ica_data.csv')
 disaster_data = pd.read_csv('cosc2669-or-cosc2186-WIL-project/datasets_cleaned/disaster-2023-cleaned.csv')
 
-# since we're only needing to predict insurance claims (for now), i just removed the states
-columns_to_remove = ['NSW', 'VIC', 'QLD', 'ACT', 'TAS', 'SA', 'NT', 'WA', 'Waters']
-disaster_data_cleaned = disaster_data.drop(columns=columns_to_remove)
+#i just removed one of the states column
+
 
 columns_to_remove = ['NSW', 'VIC', 'QLD', 'ACT', 'TAS', 'SA', 'NT', 'WA']
 ica_data_cleaned = ica_data.drop(columns=columns_to_remove)
 
-disaster_data_cleaned.columns = disaster_data_cleaned.columns.str.lower()
+disaster_data.columns = disaster_data.columns.str.lower()
+
+
 
 #i merged the datasets on event name and event start
 
 # merge on event start
 merged_start = pd.merge(
-    ica_data,
-    disaster_data_cleaned,
+    ica_data_cleaned,
+    disaster_data,
     left_on='event start',
     right_on='start date',
     how='inner'
@@ -26,8 +27,8 @@ merged_start = pd.merge(
 
 # merge on event name
 merged_name = pd.merge(
-    ica_data,
-    disaster_data_cleaned,
+    ica_data_cleaned,
+    disaster_data,
     left_on='event name',
     right_on='event',
     how='inner'
@@ -36,7 +37,7 @@ merged_name = pd.merge(
 #concat them together
 merged_data = pd.concat([merged_start, merged_name]).drop_duplicates()
 
-print(merged_data.head())
+print(merged_data.info())
 
 merged_data.to_csv('cosc2669-or-cosc2186-WIL-project/datasets_cleaned/merged_dataset.csv', index=False)
 
