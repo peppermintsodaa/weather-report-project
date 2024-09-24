5f1e2300e16d1e825f8bd63317181b6d12d719e9
peppermintsodaa <paper.miiints@gmail.com>
2024-09-24 12:01:01 +1000

    adding subsetted dataset to test model

--- model_weather.py
+++ model_weather.py
@@ -4,6 +4,9 @@ import pandas as pd
 #weather_data = pd.read_csv('datasets_cleaned/merged_weather_data.csv')
 weather_data = pd.read_csv('datasets_cleaned/merged_weather_data_tiny.csv')
 
-# get taraget and feature variables
+# get target and feature variables
+target = 
 
-features = 
+feature_ls = list(weather_data)
+feature_ls = feature_ls[:14] + feature_ls[18:20]
+features = weather_data[feature_ls]
--- weather_merging.py
+++ weather_merging.py
@@ -10,10 +10,11 @@ print(geo_data.head())
 merged_data = pd.merge(weather_data, geo_data, on=['ClusterID', 'ClusterID'], how='left')
 
 print(merged_data.head())
+print(merged_data.info())
 
-# 
+# WARNING: final dataset will be > 19 GB. please proceed with caution
 #merged_data.to_csv("datasets_cleaned/merged_weather_data.csv", index=False)
 
-# subsetted version
-merged_data.head(10000).to_csv("datasets_cleaned/merged_weather_data_tiny.csv", index=False)
-
+# subsetted version of 250,000 randomly selected  entries to test model for now
+merged_data_tiny = merged_data.sample(n = 250000, random_state = 69420)
+merged_data_tiny.to_csv("datasets_cleaned/merged_weather_data_tiny.csv", index=False)
