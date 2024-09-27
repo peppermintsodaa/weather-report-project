8fa32a4b513a0286c79aeef648d66d40aaa249d9
peppermintsodaa <paper.miiints@gmail.com>
2024-09-27 22:11:17 +1000

    created working drop down menu filtering by suburbs in dataset

--- WIL Project Website/cors_http_server.py
+++ WIL Project Website/cors_http_server.py
@@ -1,4 +1,4 @@
-# code taken from https://stackoverflow.com/a/21957017
+# code ref: https://stackoverflow.com/a/21957017
 from http.server import HTTPServer, SimpleHTTPRequestHandler, test
 import sys
 
@@ -8,4 +8,5 @@ class CORSRequestHandler (SimpleHTTPRequestHandler):
         SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
+    # bind to custom header to avoid using localhost
     test(CORSRequestHandler, HTTPServer, bind='127.0.0.1', port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
